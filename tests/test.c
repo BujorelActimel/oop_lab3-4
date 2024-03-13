f88934821e811e59d3c68a82d623b308227bbbc0
bujor <bujormihaialexandru@gmail.com>
2024-03-13 14:57:06 +0200

    test command change

--- tests/test.c
+++ tests/test.c
@@ -1,10 +1,12 @@
 #include "test_domain.h"
 #include "test_repo.h"
+#include "test_service.h"
 #include <stdio.h>
 
 int main() {
     all_domain_tests();
     all_repo_tests();
+    all_service_tests();
     printf("All tests passed!\n");
     return 0;
 }
---
+++ tests/test_service.c
@@ -0,0 +1,37 @@
+#include "../service/service.h"
+#include "../repository/repo.h"
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+void test_add_medication() {
+    repo* repository = construct_repo();
+    add_medication(repository, 1, "Paracetamol", 500, 100);
+    assert(repository->length == 1);
+    assert(repository->inventory[0]->id == 1);
+    assert(strcmp(repository->inventory[0]->name, "Paracetamol") == 0);
+    assert(repository->inventory[0]->concentration == 500);
+    assert(repository->inventory[0]->units == 100);
+    destruct_med(repository->inventory[0]);
+}
+
+void test_update_medication() {
+    repo* repository = construct_repo();
+    add_medication(repository, 1, "Paracetamol", 500, 100);
+    update_medication(repository, 1, "Paracetamol", 100);
+    assert(repository->inventory[0]->concentration == 100);
+
+    add_medication(repository, 2, "Ibuprofen", 200, 0);
+    update_medication(repository, 2, "Ibuprofen", 400);
+    // nu ar trebui sa se modifice pentru ca numarul de unitati este 0
+    assert(repository->inventory[1]->concentration == 200);
+
+    destruct_med(repository->inventory[0]);
+    destruct_med(repository->inventory[1]);
+}
+
+void all_service_tests() {
+    test_add_medication();
+    test_update_medication();
+    printf("All Service tests passed!\n");
+}
